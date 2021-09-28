8be4319eb56182b60507b1d29b05b412c23bac1c
jach11 <jach11ocn@gmail.com>
2021-09-28 17:39:25 -0400

    Initial commit

---
+++ calculator/main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    double num1, num2, output;
+    char op;
+    printf("Please enter operator (+, -, *, /):");
+    scanf("%c", &op);
+    printf("Please enter first number:");
+    scanf("%lf", &num1);
+    printf("Please enter second number:");
+    scanf("%lf", &num2);
+
+    switch(op) {
+        case '+':
+            output = num1 + num2;
+            printf("%f + %f = %f", num1, num2, output);
+            break;
+        case '-':
+            output = num1 - num2;
+            printf("%f + %f = %f", num1, num2, output);
+            break;
+        case '*':
+            output = num1 * num2;
+            printf("%f + %f = %f", num1, num2, output);
+            break;
+        case '/':
+            output = num1 / num2;
+            printf("%f + %f = %f", num1, num2, output);
+            break;
+        default:
+            printf("Error! Operator not correct!");
+    }
+    return 0;
+}
